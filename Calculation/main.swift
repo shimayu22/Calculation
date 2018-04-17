636348933c06c6224e2c7467fcad07a7c54082e1
Yutaka Shimada <shimayu22@gmail.com>
2018-04-17 22:59:38 +0900

    Intで割った時の確認

---
+++ Calculation/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Calculation
+//
+//  Created by Yutaka Shimada on 2018/04/14.
+//  Copyright © 2018年 shimayu. All rights reserved.
+//
+
+import Foundation
+
+
+for i in 0 ... 30{
+    print("\(i) : \(i/3)")
+}
+
+
